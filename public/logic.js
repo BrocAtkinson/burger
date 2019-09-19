11d6b182c584a8088ad1aa1c822fb79103226546
Broc Atkinson <bastudent41@gmail.co>
2019-09-19 17:57:44 -0600

    updated image

---
+++ public/logic.js
@@ -0,0 +1,45 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+  $(".change-devour").on("click", function(event) {
+    var id = $(this).data("id");
+    var devoured = $(this).data("devoured");
+
+    var devoured = {
+      devour: devoured
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burger/" + id, {
+      type: "PUT",
+      data: devoured
+    }).then(
+      function() {
+        console.log("changed devour to", devoured);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newburger = {
+      name: $("#ca").val().trim(),
+      cheeseburger: $("[name=cheeseburger]:checked").val().trim() || ''
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burger", {
+      type: "POST",
+      data: newburger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+});
