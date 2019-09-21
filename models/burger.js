51cacdc20e98b9b8ad759490b5428a302814be31
Broc Atkinson <bastudent41@gmail.co>
2019-09-21 09:45:33 -0600

    daily udpate

--- models/burger.js
+++ models/burger.js
@@ -1,4 +1,4 @@
-// Import the ORM to create functions that will interact with the database.
+
 var orm = require("../config/orm.js");
 
 var burger = {
@@ -7,7 +7,7 @@ var burger = {
       callBack(res);
     });
   },
-  // The variables cols and vals are arrays.
+ 
   create: function(columName, vals, cb) {
     orm.create("burgers", columName, vals, function(res) {
       cb(res);
--- public/logic.js
+++ public/logic.js
@@ -1,45 +1,45 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
+
 $(function() {
-  $(".change-devour").on("click", function(event) {
-    var id = $(this).data("id");
-    var devoured = $(this).data("devoured");
-
-    var devoured = {
-      devour: devoured
-    };
-
-    // Send the PUT request.
-    $.ajax("/api/burger/" + id, {
-      type: "PUT",
-      data: devoured
-    }).then(
-      function() {
-        console.log("changed devour to", devoured);
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
 
   $(".create-form").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
+  
     event.preventDefault();
 
-    var newburger = {
-      name: $("#ca").val().trim(),
-      cheeseburger: $("[name=cheeseburger]:checked").val().trim() || ''
+    var newBurger = {
+      name: $("#ca").val().trim() || '',
+      newBurger: $("[name=cheeseburger]:checked").val().trim()|| '',
     };
-
-    // Send the POST request.
-    $.ajax("/api/burger", {
+    $.ajax("/api/burger" ,{
       type: "POST",
-      data: newburger
+      data: newBurger
     }).then(
       function() {
         console.log("created new burger");
-        // Reload the page to get the updated list
+       
         location.reload();
       }
     );
   });
+
+  $(".Devoured").on("click", function(event) {
+    var id = $(this).data("id");
+    var devoured = $(this).data("devoured");
+console.log(id)
+    var devoured = {
+      devour: devoured,
+    function() {
+   
+      location.reload();
+    }}
+    
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+    }).then(
+      function() {
+      
+        location.reload();
+      }
+    );
+  });
+
 });
